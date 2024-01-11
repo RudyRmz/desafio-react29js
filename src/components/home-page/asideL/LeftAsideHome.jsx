9a39f7348d2e6c6e5490648870b0e9b1d0c4a238
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-01-11 11:58:53 -0600

    se trabajo en componente LinksAside y se integra a LeftAsideHome

--- src/components/home-page/asideL/LeftAsideHome.jsx
+++ src/components/home-page/asideL/LeftAsideHome.jsx
@@ -4,11 +4,13 @@ import SocialAside from "./asideL-components/SocialAside";
 import TagsAside from "./asideL-components/TagsAside";
 import DevCommunity from "./asideL-components/DevCommunity";
 import FooterAside from "./asideL-components/FooterAside";
+import LinksAside from "./asideL-components/LinksAside";
 
 export default function LeftAsideHome() {
   return (
     <aside className="  flex flex-col gap-4">
       {/* <HomeLinks /> */}
+      <LinksAside />
       <OtherLinks />
       <SocialAside />
       <TagsAside />
