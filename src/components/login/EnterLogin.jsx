af4b63d4c02f9f1c37d7f9f9c6d5145ebe9269ea
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-01-13 11:27:02 -0600

    se terminaron los estilos de EnterLogin

--- src/components/login/EnterLogin.jsx
+++ src/components/login/EnterLogin.jsx
@@ -1,7 +1,7 @@
 export default function EnterLogin() {
   return (
     <main className=" flex flex-col items-center h-screen m-0 justify-center">
-      <div className=" border border-black">
+      <div>
         <section className=" flex flex-col items-center gap-3">
           <a id="icon-index">
             <img
@@ -39,41 +39,36 @@ export default function EnterLogin() {
           <div className="d-flex justify-content-center mt-2">
             <p>OR</p>
           </div>
-          <form className=" flex flex-col border border-black w-full gap-1">
+          <form className=" flex flex-col w-full gap-1">
             <span className=" font-semibold">Email</span>
             <input
               type="text"
-              className=" border border-[#d4d4d4] w-full p-3 rounded-md"
+              className=" border border-[#d4d4d4] w-full p-2 rounded-md outline-[#3b49df]"
               required
             />
             <span className=" font-semibold">Password</span>
             <input
               type="password"
-              className=" border border-[#d4d4d4] w-full p-3 rounded-md"
+              className=" border border-[#d4d4d4] w-full p-2 rounded-md outline-[#3b49df]"
             />
 
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                defaultValue=""
-                id="flexCheckDefault"
-              />
-              <label className="form-check-label" htmlFor="flexCheckDefault">
-                Remember me
-              </label>
-              <p>
-                <a
-                  className="link-offset-2 link-underline link-underline-opacity-0"
-                  href="#"
-                >
-                  Forgot password?
-                </a>
-              </p>
+            <div className=" flex justify-between">
+              <div className=" flex gap-2">
+                <input type="checkbox" defaultValue="" />
+                <label className="form-check-label" htmlFor="flexCheckDefault">
+                  Remember me
+                </label>
+              </div>
+              <a
+                className="link-offset-2 link-underline link-underline-opacity-0"
+                href="#"
+              >
+                Forgot password?
+              </a>
             </div>
-            <div className="d-grid gap-2">
+            <div>
               <button
-                className="btn btn-primary"
+                className=" bg-[#3b49df] w-full text-white font-semibold p-3 rounded-md"
                 type="submit"
                 id="login_button"
               >
@@ -82,45 +77,42 @@ export default function EnterLogin() {
             </div>
           </form>
         </section>
-      </div>
-
-      <div className="d-flex justify-content-center mt-2 mb-3 text-center">
-        <span>
-          By signing in, you are agreeing to our{" "}
-          <a
-            className="link-offset-2 link-underline link-underline-opacity-0"
-            href="https://dev.to/privacy"
-          >
-            privacy policy
-          </a>
-          ,
-          <a
-            className="link-offset-2 link-underline link-underline-opacity-0"
-            href="https://dev.to/terms"
-          >
-            terms of use
-          </a>{" "}
-          and{" "}
-          <a
-            className="link-offset-2 link-underline link-underline-opacity-0"
-            href="https://dev.to/code-of-conduct"
-          >
-            code of conduct
-          </a>
-          .
-        </span>
-      </div>
-      <div className="d-flex justify-content-center">
-        <p>
-          New to DEV Community?{" "}
-          <a
-            className="link-offset-2 link-underline link-underline-opacity-0"
-            href="../new_user/new_user.html"
-          >
-            Create account
-          </a>
-          .
-        </p>
+        <div className=" flex flex-col items-center gap-2 pt-5">
+          <span className=" italic">
+            By signing in, you are agreeing to our{" "}
+            <a
+              className="link-offset-2 link-underline link-underline-opacity-0"
+              href="https://dev.to/privacy"
+            >
+              privacy policy
+            </a>
+            ,
+            <a
+              className="link-offset-2 link-underline link-underline-opacity-0"
+              href="https://dev.to/terms"
+            >
+              terms of use
+            </a>{" "}
+            and{" "}
+            <a
+              className="link-offset-2 link-underline link-underline-opacity-0"
+              href="https://dev.to/code-of-conduct"
+            >
+              code of conduct
+            </a>
+            .
+          </span>
+          <p>
+            New to DEV Community?{" "}
+            <a
+              className="link-offset-2 link-underline link-underline-opacity-0"
+              href="../new_user/new_user.html"
+            >
+              Create account
+            </a>
+            .
+          </p>
+        </div>
       </div>
     </main>
   );
